1306518708e03bd6406d0f739e8169f8338bedb1
jangky000 <jangky000@mme.dongguk.edu>
2020-12-29 14:38:27 +0900

    [feat] App 모듈화 App 모듈에서 App 컴포넌트 선언, Layout,Section 컴포넌트 import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SectionComponent } from './section/section.component';
-import { TimeDisplayComponent } from './section/time-display/time-display.component';
-import { ButtonsComponent } from './section/buttons/buttons.component';
+import { LayoutModule } from './layout/layout.module';
+import { SectionModule } from './section/section.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent,
-    HeaderComponent,
-    FooterComponent,
-    SectionComponent,
-    TimeDisplayComponent,
-    ButtonsComponent
+    // HelloComponent,
+    // HeaderComponent,
+    // FooterComponent,
+    // SectionComponent,
+    // TimeDisplayComponent,
+    // ButtonsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule, // -> 모듈을 가져온 상태 // import * as LayoutModule from "./layout/layout.module"
+    SectionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
